672fd53625ed4bf3bf2bff7ad3d1854c3f699b5f
nailabilqists7-ship-it <nailabilqists7@gmail.com>
2025-11-28 21:42:26 +0700

    pertemuann 9

---
+++ PRAKTIKUM10/panggil.py
@@ -0,0 +1,15 @@
+import bangunRuang as br 
+
+print("~~~ BANGUN RUANG ")
+print(f"Volume kubus dengan sisi 3 adalah {br.kubus(3)}")
+print(f"Volume balok adalah {br.balok(4, 5,2)}")
+print(f"Volume prisma segitiga adalah {br.prisma(5, 4, 5)}")
+print(f"Volume tabung adalah {br.tabung(6, 15)}")
+print(f"Volume kerucut adalah {br.kerucut(4, 14)}")
+
+print(" BANGUN DATAR~~~")
+print(f"Luas persegi adalah")
+print(f"Luas persegi panjang")
+print(f"Luas segitiga adalah")
+print(f"Luas lingkaran adalah")
+print(f"Luas jajarGenjang")
